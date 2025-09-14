3162c35d80216f1a8ce0de7cd7f80d69555a6234
SatyjeetKumar07 <satyjeetrajpoot548@gmail.com>
2025-09-14 16:47:16 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -12,7 +12,7 @@ const clientRoutes = require('./routes/clientRoutes');
 
 connectDB();
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 4000;
 // middleware/upload.js
 const multer = require('multer');
 // Configure multer for file upload
